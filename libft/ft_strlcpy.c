81746875a1e69cad7e429231e96667a333585877
Elisapdx <94479420+Elisapdx@users.noreply.github.com>
2022-11-18 18:08:06 +0100

    Add files via upload

---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epraduro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 11:49:08 by epraduro          #+#    #+#             */
+/*   Updated: 2022/11/17 15:58:53 by epraduro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	unsigned int	res;
+	unsigned int	i;
+
+	res = 0;
+	while (src[res] != '\0')
+		res++;
+	i = 0;
+	while (src[i] != '\0' && i < (dstsize - 1))
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (res);
+}
---
+++ libft/ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epraduro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/18 14:54:13 by epraduro          #+#    #+#             */
+/*   Updated: 2022/11/18 17:53:04 by epraduro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*s2;
+
+	i = 0;
+	s2 = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!s2)
+		return (NULL);
+	while (s[i])
+	{
+		s2[i] = (*f)(i, s[i]);
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
+}
